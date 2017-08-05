57b07779cce5b1809304d7989976a8072f78fa52
lgants <lgants@gmail.com>
2017-08-05 18:38:14 -0500

    added grouping controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  // FormControl - 1st argument: initial value, 2nd argument validators, 3rd argument potential async validators
+  ngOnInit() {
+    // don't invoke Validator methods such as required, only pass the reference and angular will execute when input changes
+    this.signupForm = new FormGroup({
+      'userData': new FormGroup({
+        'username': new FormControl(null, Validators.required),
+        'email': new FormControl(null, [Validators.required, Validators.email])
+      }),
+      'gender': new FormControl('male'),
+    });
+  }
+
+  onSubmit() {
+
+  }
 }
