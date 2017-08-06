58b6c0a22eaf816f7367d0d057574c639b426b7c
lgants <lgants@gmail.com>
2017-08-05 19:59:51 -0500

    added notes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -18,15 +19,41 @@ export class AppComponent implements OnInit {
     this.signupForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'email': new FormControl(null, [Validators.required, Validators.email])
+        'email': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
+    // valueChanges is an observable on the form
+    // this.signupForm.valueChanges.subscribe(
+    //   (value) => console.log(value)
+    // )
+    // statusChanges is an observable on the form
+    this.signupForm.statusChanges.subscribe(
+      (value) => console.log(value)
+    )
+    // NOTE setValue and patchValue still available on responsive form
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: 'Logan',
+    //     email: 'logan@test.com'
+    //   },
+    //   gender: 'male',
+    //   hobbies: []
+    // });
+    // this.signupForm.patchValue({
+    //   userData: {
+    //     username: 'Logan',
+    //     email: 'logan@test.com'
+    //   },
+    //   gender: 'male',
+    //   hobbies: []
+    // });
   }
 
   onSubmit() {
-
+    // NOTE reset also available on reactive forms
+    // this.signupForm.reset();
   }
 
   onAddHobby() {
@@ -37,10 +64,24 @@ export class AppComponent implements OnInit {
   // tells TS key should be type string and value should be type boolean
   // if validation is successful, it should return null
   forbiddenNames(control: FormControl): {[s: string]: boolean} {
-    if (this.forbiddenUsernames.indexOf(control.value)) {
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
       return {'nameIsForbidden': true};
     } else {
       null;
     }
+  }
+
+  // Promise and Observable are constructs that handle async data
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'test@test.com') {
+          resolve({'emailIsForbidden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
